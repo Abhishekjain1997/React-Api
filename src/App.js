e69277619b24600fa9e9116ec82954b0d49c696c
Abhishekjain1997 <abhishekjain2589@gmail.com>
2024-02-26 14:41:00 +0530

    sending-post-request

--- src/App.js
+++ src/App.js
@@ -20,15 +20,16 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovie=[];
+      for(const key in data){
+        loadedMovie.push({
+          id:key,
+          title:data[key].title,
+          openingText:data[key].openingText,
+          releaseDate:data[key].releaseDate
+        });
+      }
+      setMovies(loadedMovie);
     } catch (error) {
       setError(error.message);
     }
@@ -39,8 +40,16 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+   const response=await fetch('https://react-http-14849-default-rtdb.firebaseio.com/movies.json',{
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers:{
+        'Content-Type':'applicatin/json'
+      } 
+    });
+    const data=await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
